1c7f9a9acd763423aa1c86c6dc7220ad349b5f3b
Kruce Blake <kruceblake@gmail.com>
2020-04-24 19:08:05 -0400

    moved custom css into site.css and added minified; added cheat; updated typewriter from injecting css styles into body; updated layout;

--- KruceBlakeSite/wwwroot/js/typewriter.js
+++ KruceBlakeSite/wwwroot/js/typewriter.js
@@ -48,8 +48,4 @@ window.onload = function () {
             new TxtType(elements[i], JSON.parse(toRotate), period);
         }
     }
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
 };
