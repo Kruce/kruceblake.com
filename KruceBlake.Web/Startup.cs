8cd1435cefbfb2afa28d4979add600d472b190be
Kruce Blake <kruceblake@gmail.com>
2020-04-26 02:28:27 -0400

    removed packages that were causing a lot of unnecessary files on publish; updated gitignore to include configs build folder;

--- KruceBlake.Web/Startup.cs
+++ KruceBlake.Web/Startup.cs
@@ -1,10 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,10 +21,10 @@ namespace KruceBlake.Web
         public void ConfigureServices(IServiceCollection services)
         {
             IMvcBuilder builder = services.AddControllersWithViews();
-            if (Env.IsDevelopment())
-            {
-                builder.AddRazorRuntimeCompilation();
-            }
+            //if (Env.IsDevelopment())
+            //{
+            //    builder.AddRazorRuntimeCompilation();
+            //}
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
