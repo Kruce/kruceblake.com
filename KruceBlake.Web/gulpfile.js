2c4f16fcb67e76c939239277d7807a33c3a64308
Kruce Blake <kruceblake@gmail.com>
2020-04-26 23:25:57 -0400

    updated resources outside of development to use cdn if available, and fallback to local if there are issues; restructured css and sass; added bundle config and rewrote gulp tasks to compile and bundle css/scss/javascript;

--- KruceBlake.Web/gulpfile.js
+++ KruceBlake.Web/gulpfile.js
@@ -1,123 +1,128 @@
-"use strict";
+`use strict`;
 
-// Load plugins
-const autoprefixer = require("gulp-autoprefixer");
-const cleanCSS = require("gulp-clean-css");
-const del = require("del");
-const gulp = require("gulp");
-const header = require("gulp-header");
-const merge = require("merge-stream");
-const plumber = require("gulp-plumber");
-const rename = require("gulp-rename");
-const sass = require("gulp-sass");
-const uglify = require("gulp-uglify");
+const autoprefixer = require(`gulp-autoprefixer`),
+    cssmin = require('gulp-cssmin'),
+    concat = require(`gulp-concat`),
+    del = require(`del`),
+    gulp = require(`gulp`),
+    header = require(`gulp-header`),
+    merge = require(`merge-stream`),
+    plumber = require(`gulp-plumber`),
+    rename = require(`gulp-rename`),
+    sass = require(`gulp-sass`),
+    uglify = require(`gulp-uglify-es`).default,
+    pkg = require(`./package.json`), // Load package.json for banner
+    banner = [`/*!\n`, // Set the banner content
+        ` * kruceblake.com - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n`,
+        ` * Copyright 2020-` + (new Date()).getFullYear(), ` <%= pkg.author %>\n`,
+        ` */\n`,
+        `\n`
+    ].join(``),
+    regex = { //regex for identifying file types
+        css: /\.css$/,
+        html: /\.(html|htm)$/,
+        js: /\.js$/,
+        sass: /\.scss$/
+    },
+    bundleconfig = require(`./bundleconfig.json`),
+    getBundles = (regexPattern) => { //gets all bundles by given regex
+        return bundleconfig.filter(bundle => {
+            return regexPattern.test(bundle.outputFileName);
+        });
+    };
 
-// Load package.json for banner
-const pkg = require('./package.json');
+///minifies javascript
+gulp.task('min:js', async function () {
+    merge(getBundles(regex.js).map(bundle => {
+        return gulp.src(bundle.inputFiles, { base: '.' })
+            .pipe(concat(bundle.outputFileName, { newLine: '' }))
+            .pipe(uglify())
+            .pipe(gulp.dest('.'));
+    }))
+});
 
-// Set the banner content
-const banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' */\n',
-    '\n'
-].join('');
-
-// Clean lib
-function clean() {
-    return del(["./wwwroot/lib/"]);
-}
-
-// Bring third party dependencies from node_modules into lib directory
-function modules() {
-    // Bootstrap
-    var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-        .pipe(gulp.dest('./wwwroot/lib/bootstrap'));
-    // Font Awesome CSS
-    var fontAwesomeCSS = gulp.src('./node_modules/@fortawesome/fontawesome-free/css/**/*')
-        .pipe(gulp.dest('./wwwroot/lib/fontawesome-free/css'));
-    // Font Awesome Webfonts
-    var fontAwesomeWebfonts = gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/**/*')
-        .pipe(gulp.dest('./wwwroot/lib/fontawesome-free/webfonts'));
-    // jQuery Easing
-    var jqueryEasing = gulp.src('./node_modules/jquery.easing/*.js')
-        .pipe(gulp.dest('./wwwroot/lib/jquery-easing'));
-    // jQuery
-    var jquery = gulp.src([
-        './node_modules/jquery/dist/*',
-        '!./node_modules/jquery/dist/core.js'
-    ]).pipe(gulp.dest('./wwwroot/lib/jquery'));
-    // AOS
-    var aos = gulp.src('./node_modules/aos/dist/**/*')
-        .pipe(gulp.dest('./wwwroot/lib/aos'));
-    // jQuery validation unobtrusive
-    var jqueryValidationUnobtrusive = gulp.src('./node_modules/jquery-validation-unobtrusive/dist/**/*')
-        .pipe(gulp.dest('./wwwroot/lib/jquery-validation-unobtrusive'));
-    // jQuery validation
-    var jqueryValidation = gulp.src([
-        './node_modules/jquery-validation/dist/*',
-        '!./node_modules/jquery-validation/dist/localization'
-    ]).pipe(gulp.dest('./wwwroot/lib/jquery-validation'));
-    return merge(bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jqueryEasing, jquery, aos, jqueryValidationUnobtrusive, jqueryValidation);
-}
-
-// CSS task
-function css() {
+///compiles sass
+gulp.task(`min:sass`, async function () {
     return gulp
-        .src("./wwwroot/scss/**/*.scss")
+        .src(`./wwwroot/scss/**/*.scss`)
         .pipe(plumber())
         .pipe(sass({
-            outputStyle: "expanded",
-            includePaths: "./node_modules",
+            outputStyle: `expanded`,
+            includePaths: `./node_modules`,
         }))
-        .on("error", sass.logError)
+        .on(`error`, sass.logError)
         .pipe(autoprefixer({
             cascade: false
         }))
         .pipe(header(banner, {
             pkg: pkg
         }))
-        .pipe(gulp.dest("./wwwroot/css"))
+        .pipe(gulp.dest(`./wwwroot/css`))
         .pipe(rename({
-            suffix: ".min"
+            suffix: `.min`
         }))
-        .pipe(cleanCSS())
-        .pipe(gulp.dest("./wwwroot/css"));
-}
+        .pipe(cssmin())
+        .pipe(gulp.dest(`./wwwroot/css`));
+});
 
-// JS task
-function js() {
-    return gulp
-        .src([
-            './wwwroot/js/*.js',
-            '!./wwwroot/js/*.min.js'
-        ])
-        .pipe(uglify())
-        .pipe(header(banner, {
-            pkg: pkg
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        }))
-        .pipe(gulp.dest('./wwwroot/js'));
-}
+///minifies css
+gulp.task(`min:css`, async function () {
+    merge(getBundles(regex.css).map(bundle => {
+        return gulp.src(bundle.inputFiles, { base: `.` })
+            .pipe(concat(bundle.outputFileName, { newLine: '' }))
+            .pipe(cssmin({keepSpecialComments: 1})) //keepSpecialComments changes in updates
+            .pipe(gulp.dest(`.`));
+    }))
+});
 
-// Watch files
-function watchFiles() {
-    gulp.watch("./wwwroot/scss/**/*", css);
-    gulp.watch(["./wwwroot/js/**/*", "!./wwwroot/js/**/*.min.js"], js);
-}
+///minifies sass, css and javascript
+gulp.task(`min`, gulp.series([`min:js`, `min:sass`, `min:css`]));
 
-// Define complex tasks
-const lib = gulp.series(clean, modules);
-const build = gulp.series(lib, gulp.parallel(css, js));
-const watch = gulp.series(build, gulp.parallel(watchFiles));
+///Deletes lib folder then redownloads all modules
+gulp.task(`modules`, async function () {
+    // Bootstrap
+    var bootstrap = gulp.src(`./node_modules/bootstrap/dist/**/*`)
+        .pipe(gulp.dest(`./wwwroot/lib/bootstrap`));
+    // Font Awesome CSS
+    var fontAwesomeCSS = gulp.src(`./node_modules/@fortawesome/fontawesome-free/css/**/*`)
+        .pipe(gulp.dest(`./wwwroot/lib/fontawesome-free/css`));
+    // Font Awesome Webfonts
+    var fontAwesomeWebfonts = gulp.src(`./node_modules/@fortawesome/fontawesome-free/webfonts/**/*`)
+        .pipe(gulp.dest(`./wwwroot/lib/fontawesome-free/webfonts`));
+    // jQuery Easing
+    var jqueryEasing = gulp.src(`./node_modules/jquery.easing/*.js`)
+        .pipe(gulp.dest(`./wwwroot/lib/jquery-easing`));
+    // jQuery
+    var jquery = gulp.src([
+        `./node_modules/jquery/dist/*`,
+        `!./node_modules/jquery/dist/core.js`
+    ]).pipe(gulp.dest(`./wwwroot/lib/jquery`));
+    // AOS
+    var aos = gulp.src(`./node_modules/aos/dist/**/*`)
+        .pipe(gulp.dest(`./wwwroot/lib/aos`));
+    // jQuery validation unobtrusive
+    var jqueryValidationUnobtrusive = gulp.src(`./node_modules/jquery-validation-unobtrusive/dist/**/*`)
+        .pipe(gulp.dest(`./wwwroot/lib/jquery-validation-unobtrusive`));
+    // jQuery validation
+    var jqueryValidation = gulp.src([
+        `./node_modules/jquery-validation/dist/*`,
+        `!./node_modules/jquery-validation/dist/localization`
+    ]).pipe(gulp.dest(`./wwwroot/lib/jquery-validation`));
+    merge(del([`./wwwroot/lib/`]), bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jqueryEasing, jquery, aos, jqueryValidationUnobtrusive, jqueryValidation);
+})
 
-// Export tasks
-exports.css = css;
-exports.js = js;
-exports.clean = clean;
-exports.lib = lib;
-exports.build = build;
-exports.watch = watch;
-exports.default = build;
+///watches files and runs task if changes occur
+gulp.task('watch', () => {
+    getBundles(regex.js).forEach(
+        bundle => gulp.watch(bundle.inputFiles, gulp.series(["min:js"])));
+    gulp.watch(`./wwwroot/scss/**/*`, gulp.series([`min:sass`, `min:css`]));
+    getBundles(regex.css).forEach(
+        bundle => gulp.watch(bundle.inputFiles, gulp.series(["min:css"])));
+});
+
+///cleans any task files
+gulp.task('clean', () => {
+    return del(bundleconfig.map(bundle => bundle.outputFileName));
+});
+
+gulp.task('default', gulp.series("min"));
