84c0aded2c15e8a060e068c002d7bf91b04322d1
Kruce Blake <kruceblake@gmail.com>
2020-04-29 02:02:31 -0400

    complete restructure of wwwroot to account for updated gulp task bundling and minifying; gulpfile tasks rewritten and modified to build files to proper source and dist folder; dist folder is meant to be published and source folder is meant just for production; removed bundleconfig as it does not serve any purpose with updated gulp tasks;

--- KruceBlake.Web/gulpfile.js
+++ KruceBlake.Web/gulpfile.js
@@ -1,14 +1,15 @@
 `use strict`;
 
 const autoprefixer = require(`gulp-autoprefixer`),
-    cssmin = require('gulp-cssmin'),
+    distPath = `./wwwroot/dist`,
+    sourcePath = `./wwwroot/source`,
+    cssmin = require(`gulp-cssmin`),
     concat = require(`gulp-concat`),
     del = require(`del`),
     gulp = require(`gulp`),
     header = require(`gulp-header`),
     merge = require(`merge-stream`),
     plumber = require(`gulp-plumber`),
-    rename = require(`gulp-rename`),
     sass = require(`gulp-sass`),
     uglify = require(`gulp-uglify-es`).default,
     pkg = require(`./package.json`), // Load package.json for banner
@@ -17,34 +18,20 @@ const autoprefixer = require(`gulp-autoprefixer`),
         ` * Copyright 2020-` + (new Date()).getFullYear(), ` <%= pkg.author %>\n`,
         ` */\n`,
         `\n`
-    ].join(``),
-    regex = { //regex for identifying file types
-        css: /\.css$/,
-        html: /\.(html|htm)$/,
-        js: /\.js$/,
-        sass: /\.scss$/
-    },
-    bundleconfig = require(`./bundleconfig.json`),
-    getBundles = (regexPattern) => { //gets all bundles by given regex
-        return bundleconfig.filter(bundle => {
-            return regexPattern.test(bundle.outputFileName);
-        });
-    };
+    ].join(``);
 
 ///minifies javascript
-gulp.task('min:js', async function () {
-    merge(getBundles(regex.js).map(bundle => {
-        return gulp.src(bundle.inputFiles, { base: '.' })
-            .pipe(concat(bundle.outputFileName, { newLine: '' }))
-            .pipe(uglify())
-            .pipe(gulp.dest('.'));
-    }))
+gulp.task(`min:js`, async function () {
+    return gulp.src([`${sourcePath}/js/**/*.js`, `!${sourcePath}/js/**/*.min.js`])
+        .pipe(concat(`site.min.js`, { newLine: `` }))
+        .pipe(uglify())
+        .pipe(gulp.dest(`${distPath}/js/`));
 });
 
 ///compiles sass
-gulp.task(`min:sass`, async function () {
+gulp.task(`min:sass`, function () { //leaving this task synchronus for the series where we need this to finish before minifying css.
     return gulp
-        .src(`./wwwroot/scss/**/*.scss`)
+        .src(`${sourcePath}/scss/**/*.scss`)
         .pipe(plumber())
         .pipe(sass({
             outputStyle: `expanded`,
@@ -57,72 +44,47 @@ gulp.task(`min:sass`, async function () {
         .pipe(header(banner, {
             pkg: pkg
         }))
-        .pipe(gulp.dest(`./wwwroot/css`))
-        .pipe(rename({
-            suffix: `.min`
-        }))
-        .pipe(cssmin())
-        .pipe(gulp.dest(`./wwwroot/css`));
+        .pipe(gulp.dest(`${sourcePath}/css`));
 });
 
-///minifies css
+///minifies javascript
 gulp.task(`min:css`, async function () {
-    merge(getBundles(regex.css).map(bundle => {
-        return gulp.src(bundle.inputFiles, { base: `.` })
-            .pipe(concat(bundle.outputFileName, { newLine: '' }))
-            .pipe(cssmin({keepSpecialComments: 1})) //keepSpecialComments changes in updates
-            .pipe(gulp.dest(`.`));
-    }))
+    return gulp.src([`${sourcePath}/css/**/*.css`, `!${sourcePath}/css/**/*.min.css`])
+        .pipe(concat(`site.min.css`, { newLine: `` }))
+        .pipe(cssmin())
+        .pipe(gulp.dest(`${distPath}/css`));
+});
+
+///clears lib folder and redownloads all necessary modules
+gulp.task(`modules`, async function () {
+    // Bootstrap
+    var bootstrap = gulp.src(`./node_modules/bootstrap/dist/**/*`).pipe(gulp.dest(`${distPath}/lib/bootstrap`));
+    // Font Awesome CSS
+    var fontAwesomeCSS = gulp.src(`./node_modules/@fortawesome/fontawesome-free/css/**/*`).pipe(gulp.dest(`${distPath}/lib/fontawesome-free/css`));
+    // Font Awesome Webfonts
+    var fontAwesomeWebfonts = gulp.src(`./node_modules/@fortawesome/fontawesome-free/webfonts/**/*`).pipe(gulp.dest(`${distPath}/lib/fontawesome-free/webfonts`));
+    // jQuery Easing
+    var jqueryEasing = gulp.src(`./node_modules/jquery.easing/*.js`).pipe(gulp.dest(`${distPath}/lib/jquery-easing`));
+    // jQuery
+    var jquery = gulp.src([`./node_modules/jquery/dist/*`, `!./node_modules/jquery/dist/core.js`]).pipe(gulp.dest(`${distPath}/lib/jquery`));
+    // AOS
+    var aos = gulp.src(`./node_modules/aos/dist/**/*`).pipe(gulp.dest(`${distPath}/lib/aos`));
+    // jQuery validation unobtrusive
+    var jqueryValidationUnobtrusive = gulp.src(`./node_modules/jquery-validation-unobtrusive/dist/**/*`).pipe(gulp.dest(`${distPath}/lib/jquery-validation-unobtrusive`));
+    // jQuery validation
+    var jqueryValidation = gulp.src([`./node_modules/jquery-validation/dist/*`, `!./node_modules/jquery-validation/dist/localization`]).pipe(gulp.dest(`${distPath}/lib/jquery-validation`));
+
+    merge(del([`${distPath}/lib/`]), bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jqueryEasing, jquery, aos, jqueryValidationUnobtrusive, jqueryValidation);
 });
 
 ///minifies sass, css and javascript
 gulp.task(`min`, gulp.series([`min:js`, `min:sass`, `min:css`]));
 
-///Deletes lib folder then redownloads all modules
-gulp.task(`modules`, async function () {
-    // Bootstrap
-    var bootstrap = gulp.src(`./node_modules/bootstrap/dist/**/*`)
-        .pipe(gulp.dest(`./wwwroot/lib/bootstrap`));
-    // Font Awesome CSS
-    var fontAwesomeCSS = gulp.src(`./node_modules/@fortawesome/fontawesome-free/css/**/*`)
-        .pipe(gulp.dest(`./wwwroot/lib/fontawesome-free/css`));
-    // Font Awesome Webfonts
-    var fontAwesomeWebfonts = gulp.src(`./node_modules/@fortawesome/fontawesome-free/webfonts/**/*`)
-        .pipe(gulp.dest(`./wwwroot/lib/fontawesome-free/webfonts`));
-    // jQuery Easing
-    var jqueryEasing = gulp.src(`./node_modules/jquery.easing/*.js`)
-        .pipe(gulp.dest(`./wwwroot/lib/jquery-easing`));
-    // jQuery
-    var jquery = gulp.src([
-        `./node_modules/jquery/dist/*`,
-        `!./node_modules/jquery/dist/core.js`
-    ]).pipe(gulp.dest(`./wwwroot/lib/jquery`));
-    // AOS
-    var aos = gulp.src(`./node_modules/aos/dist/**/*`)
-        .pipe(gulp.dest(`./wwwroot/lib/aos`));
-    // jQuery validation unobtrusive
-    var jqueryValidationUnobtrusive = gulp.src(`./node_modules/jquery-validation-unobtrusive/dist/**/*`)
-        .pipe(gulp.dest(`./wwwroot/lib/jquery-validation-unobtrusive`));
-    // jQuery validation
-    var jqueryValidation = gulp.src([
-        `./node_modules/jquery-validation/dist/*`,
-        `!./node_modules/jquery-validation/dist/localization`
-    ]).pipe(gulp.dest(`./wwwroot/lib/jquery-validation`));
-    merge(del([`./wwwroot/lib/`]), bootstrap, fontAwesomeCSS, fontAwesomeWebfonts, jqueryEasing, jquery, aos, jqueryValidationUnobtrusive, jqueryValidation);
-})
-
 ///watches files and runs task if changes occur
-gulp.task('watch', () => {
-    getBundles(regex.js).forEach(
-        bundle => gulp.watch(bundle.inputFiles, gulp.series(["min:js"])));
-    gulp.watch(`./wwwroot/scss/**/*`, gulp.series([`min:sass`, `min:css`]));
-    getBundles(regex.css).forEach(
-        bundle => gulp.watch(bundle.inputFiles, gulp.series(["min:css"])));
+gulp.task(`watch`, () => {
+    gulp.watch([`${sourcePath}/js/**/*.js`, `!${sourcePath}/js/**/*.min.js`], gulp.series([`min:js`]));
+    gulp.watch([`${sourcePath}/scss/**/*`, `${sourcePath}/css/**/*.css`, `!${sourcePath}/js/**/*.min.css`,], gulp.series([`min:sass`, `min:css`]));
 });
 
-///cleans any task files
-gulp.task('clean', () => {
-    return del(bundleconfig.map(bundle => bundle.outputFileName));
-});
-
-gulp.task('default', gulp.series("min"));
+//default task that installs modules and minifies files
+gulp.task(`default`, gulp.series(`min`));
