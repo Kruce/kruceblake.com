7b5a9e1d1d0aeeb4fba37f81f7f5ac0ec7c06c35
Kruce Blake <KruceBlake@gmail.com>
2025-12-30 23:33:14 -0500

    added video pause when not in viewport or if page is hidden for background video.

---
+++ KruceBlake.Web/wwwroot/source/js/video-pause.js
@@ -0,0 +1,48 @@
+﻿const video = document.getElementById(`backgroundVideo`);
+
+//1. pause video using intersection observer api 
+const handleIntersection = (entries, observer) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) { //video is visible in viewport
+            video.play();
+        } else { //video is not visible in viewport
+            video.pause();
+        }
+    });
+};
+
+const observer = new IntersectionObserver(handleIntersection,
+    { //options
+        root: null, // relative to the viewport
+        rootMargin: `0px`,
+        threshold: 0.05
+    });
+
+observer.observe(video);
+
+//2. pause video when page is hidden using page visibility api
+let hidden, visibilityChange;
+if (typeof document.hidden !== "undefined") {
+    hidden = "hidden";
+    visibilityChange = "visibilitychange";
+} else if (typeof document.msHidden !== "undefined") {
+    hidden = "msHidden";
+    visibilityChange = "msvisibilitychange";
+} else if (typeof document.webkitHidden !== "undefined") {
+    hidden = "webkitHidden";
+    visibilityChange = "webkitvisibilitychange";
+}
+
+// pause video if page is hidden
+function handleVisibilityChange() {
+    if (document[hidden]) {
+        video.pause();
+    } else {
+        video.play();
+    }
+}
+
+// add an event listener if supported
+if (typeof document.addEventListener !== "undefined" && hidden !== undefined) {
+    document.addEventListener(visibilityChange, handleVisibilityChange, false);
+}
